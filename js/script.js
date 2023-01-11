826d72f9cb2dc96be54be1eb47efd03f03d0772e
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-01-11 16:27:00 +0100

    FINITO CON BONUS (Senza cartella bonus)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let _Container = document.getElementById('container');
 let _Play = document.getElementById('play');
 let _Grid = document.getElementById('grid');
+let _Mode = document.getElementById('mode');
 
 _Play.addEventListener('click',
 
@@ -8,15 +9,92 @@ _Play.addEventListener('click',
 
         _Container.style.display = 'block';
 
-        for (i=1; i <= 100; i++) {
+        if (_Mode.value == 'easy') {
+
+            for (i=1; i <= 100; i++) {
             
-            let _NewCell = document.createElement('div');
-            _NewCell.classList.add('cell');
-            _NewCell.innerHTML = [i];
-            _Grid.append(_NewCell);
-            
-            
+                let _NewCell = document.createElement('div');
+                _NewCell.classList.add('easy');
+                _NewCell.innerHTML = [i];
+    
+                _NewCell.addEventListener('click',
+    
+                    function() {
+                        
+                        if (this.classList.contains('clicked')) {
+    
+                            this.classList.remove('clicked');
+    
+                        } else {
+    
+                            this.classList.add('clicked');
+    
+                        }
+                    }
+                )
+    
+                _Grid.append(_NewCell);
+                    
+            }
         }
 
+        if (_Mode.value == 'medium') {
+
+            for (i=1; i <= 81; i++) {
+            
+                let _NewCell = document.createElement('div');
+                _NewCell.classList.add('medium');
+                _NewCell.innerHTML = [i];
+    
+                _NewCell.addEventListener('click',
+    
+                    function() {
+                        
+                        if (this.classList.contains('clicked')) {
+    
+                            this.classList.remove('clicked');
+    
+                        } else {
+    
+                            this.classList.add('clicked');
+    
+                        }
+                    }
+                )
+    
+                _Grid.append(_NewCell);
+                    
+            }
+        }
+
+        if (_Mode.value == 'hard') {
+
+            for (i=1; i <= 49; i++) {
+            
+                let _NewCell = document.createElement('div');
+                _NewCell.classList.add('hard');
+                _NewCell.innerHTML = [i];
+            
+                _NewCell.addEventListener('click',
+            
+                    function() {
+                        
+                        if (this.classList.contains('clicked')) {
+            
+                            this.classList.remove('clicked');
+            
+                        } else {
+            
+                            this.classList.add('clicked');
+            
+                        }
+                    }
+                )
+            
+                _Grid.append(_NewCell);
+                    
+            }
+        }
     }
 );
+
